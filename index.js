c282c0f59812bce6f888040319704fbc3c5c115f
Eric Kenney <ekenney5@gmail.com>
2016-05-10 22:32:31 -0600

    Added media files

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+
+app.use('/media', express.static(__dirname + '/media'));
+
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
